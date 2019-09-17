2762f28c5657ffa678a6a1f6a9e8e17a43a24e5f
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-17 12:03:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,15 +2,26 @@ class Shoe
   
   attr_accessor :brand, :color, :size, :material
   attr_reader :condition
-  def initialize(condition)
-    @condition = condition 
+  def initialize(condition, brand)
+    @condition = condition
+    @brand = brand
   end 
+  
+  def brand=(brand)
+    @brand = brand 
+  end 
+  def brand 
+    @brand 
+  end 
+  
   def condition=(condition)
     @condition = condition 
   end 
+  
   def condition
     @condition
   end
+  
   def cobble(condition)
     @cobble == new
   end 
